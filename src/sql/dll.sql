6f9fcf552fd5aa33a9ddc8c9ce57736a543b1b7b
Sergey Simakov <SaneniRem@yandex.ru>
2025-07-03 13:48:25 +0300

    шаг 4 готов

--- src/sql/dll.sql
+++ src/sql/dll.sql
@@ -11,3 +11,22 @@ ORDER BY group_id, user_id
 SEGMENTED BY hash(group_id) ALL NODES
 PARTITION BY event_datetime::date
 GROUP BY calendar_hierarchy_day(event_datetime::date, 3, 2);
+
+DROP TABLE IF EXISTS STV202506163__DWH.l_user_group_activity;
+
+CREATE TABLE STV202506163__DWH.l_user_group_activity
+(
+    hk_l_user_group_activity INT PRIMARY KEY,
+    hk_user_id INT NOT NULL,
+    hk_group_id INT NOT NULL,
+    load_dt DATETIME NOT NULL,
+    load_src VARCHAR(20)
+);
+
+ALTER TABLE STV202506163__DWH.l_user_group_activity
+    ADD CONSTRAINT fk_uga_users_hk_user_id
+    FOREIGN KEY (hk_user_id) REFERENCES STV202506163__DWH.h_users(hk_user_id);
+
+ALTER TABLE STV202506163__DWH.l_user_group_activity
+    ADD CONSTRAINT fk_uga_groups_hk_group_id
+    FOREIGN KEY (hk_group_id) REFERENCES STV202506163__DWH.h_groups(hk_group_id);
